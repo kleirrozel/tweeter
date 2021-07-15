88e8ce7409f35463d38200bdb2cc40a428ca9cba
Kleir Miranda <kleirmiranda@gmail.com>
2021-07-14 21:46:47 -0400

    Transferred submit handler to client.js file from form.js to keep everything in one file.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -47,6 +47,26 @@ $(document).ready(function() {
   // renderTweets(tweetData); // Removed because tweets will be loaded dynamically through the API
 
 
+/* Add an event listener that listens for the *submit* event and
+prevents the default behaviours: data submission & page refresh.
+Create an AJAX post request that sends the serialized data to the server to the server.
+*/
+  $("#form-id").submit(function(event) {
+    event.preventDefault();
+    const $form = $(this).serialize();
+
+    // console.log("THIS IS THE NEW CL:", $("#text").val().length)
+    if ($("#text").val().length > 140) {
+      return alert("Yikes! You hummed too much. Try humming a little less.");
+    } else if ($("#text").val().length === 0) {
+      return alert("I hear nothing. Type to start humming.");
+    } else {
+      $.post("/tweets", $form).done(function() {
+        // console.log("I want to see this in the console when a form is successfully submitted")
+      })
+    }
+  });
+
   /* Responsible for fetching tweets 
   from the http://localhost:8080/tweets page.
   -- It should be requesting and handling 
